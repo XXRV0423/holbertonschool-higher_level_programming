258816dd39ed87f7dabfd04f16d3806d8aede9d2
XXRV0423 <xxavierrv@gmail.com>
2026-05-05 17:42:21 -0400

    Created 5 variables to be printed in a sentence

---
+++ python-hello_world/0-print_sentence.py
@@ -0,0 +1,7 @@
+name = "Xander"
+age = 20
+fav_game = "Rainbow Six Siege"
+city = "Aguada"
+year = 2006
+
+print(f"My name is {name}, I am {age} years old, my favorite game is {fav_game}, I live in {city} and I was born in {year}.")
