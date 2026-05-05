1468998b972da4403836951ed9434e042c0f8bb9
XXRV0423 <xxavierrv@gmail.com>
2026-05-05 17:42:56 -0400

    Script that takes 3 inputs and outputs a Welcome Card

---
+++ python-hello_world/1-welcome_card.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")
+age = int(input("What is your age? "))
+fav_subject = input("What is your favorite subject? ")
+
+print(f"\n====================== Welcome Card ======================\n")
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Favorite Subject: {fav_subject}")
