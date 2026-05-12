184e4954c4006c25b927cf94574f6031d6251e9e
XXRV0423 <xxavierrv@gmail.com>
2026-05-12 18:23:44 -0400

    style: fix pycodestyle issues on 1-element_at.py

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -9,6 +9,8 @@ Args:
 Returns:
     The element at the specified index, or None if the index is out of bounds.
 """
+
+
 def element_at(my_list, idx):
     if 0 <= idx < len(my_list):
         return my_list[idx]
