da09c91ac28a611f61fa83eeb2d413d1519a231d
XXRV0423 <xxavierrv@gmail.com>
2026-05-12 18:19:25 -0400

    Function that retrieves an element from a list

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Retrieve an element from a list.
+
+Args:
+    my_list (list): A list of integers.
+    idx (int): The index of the element to retrieve.
+
+Returns:
+    The element at the specified index, or None if the index is out of bounds.
+"""
+def element_at(my_list, idx):
+    if 0 <= idx < len(my_list):
+        return my_list[idx]
+    else:
+        return None
