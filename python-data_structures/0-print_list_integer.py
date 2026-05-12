8aea5d19f505a132cb3ceca6b175f7515f96e548
Xander Roldan <115243474+XXRV0423@users.noreply.github.com>
2026-05-12 18:06:16 -0400

    Clean up docstring in print_list_integer function
    Removed redundant docstring from the print_list_integer function.

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+
+"""Print all integers of a list.
+
+Args:
+    my_list (list): A list of integers.
+
+Returns:
+    None
+"""
+    
 def print_list_integer(my_list=[]):
-    """Print all integers of a list."""
     for i in my_list:
         print("{:d}".format(i))
