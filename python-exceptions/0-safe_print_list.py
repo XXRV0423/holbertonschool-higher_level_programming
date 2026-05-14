d94ac6cea6e24cd4046d454f7057ca533e013476
XXRV0423 <xxavierrv@gmail.com>
2026-05-14 10:50:51 -0400

    README file added

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -12,11 +12,13 @@ Returns:
 
 def safe_print_list(my_list=[], x=0):
     count = 0
-    for i in range(x):
-        try:
+
+    try:
+        for i in range(x):
             print(my_list[i], end="")
             count += 1
-        except IndexError:
-            break
+    except IndexError:
+        pass
+
     print()
     return count
