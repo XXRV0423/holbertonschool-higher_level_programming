a1ae898cab6cfc9132c2f3ba05d7e43483e46ac1
XXRV0423 <xxavierrv@gmail.com>
2026-05-21 15:50:07 -0400

    style: fix pycodestyle issues in 8-rectangle.py

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -79,7 +79,7 @@ class Rectangle:
         """Print a message when the instance is deleted."""
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
-        
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """Return the rectangle with the greater or equal area."""
