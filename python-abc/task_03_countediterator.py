6bb0f90afcc86b10a09fac09ec3e3faa1b7a566f
XXRV0423 <xxavierrv@gmail.com>
2026-05-28 16:33:54 -0400

    feat: add CountedIterator class with iteration counter

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module that defines a CountedIterator class."""
+
+class CountedIterator:
+    """An iterator that counts iterated items."""
+
+    def __init__(self, iterable):
+        """Initializes the iterator and counter."""
+        self._iterator = iter(iterable)
+        self._count = 0
+
+    def __iter__(self):
+        """Returns the iterator object itself."""
+        return self
+
+    def __next__(self):
+        """Returns the next item and increments the counter."""
+        item = next(self._iterator)
+        self._count += 1
+        return item
+
+    def get_count(self):
+        """Returns the number of iterated items."""
+        return self._count
