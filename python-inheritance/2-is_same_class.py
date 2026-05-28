c462075d58b67c571cd813955181cfaacffea9b1
XXRV0423 <xxavierrv@gmail.com>
2026-05-27 23:11:59 -0400

    feat: add function to check exact class instance

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that defines a function for checking exact class"""
+
+
+def is_same_class(obj, a_class):
+    """Returns True if the obj is exactly an instance of the specified class, otherwise False."""
+    return type(obj) is a_class
