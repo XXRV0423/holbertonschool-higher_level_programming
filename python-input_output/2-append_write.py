56be5165cbd716db7734e1194ae264a404d6a4cb
XXRV0423 <xxavierrv@gmail.com>
2026-06-04 11:10:04 -0400

    Add function to append text to UTF-8 files

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that appends a string to a text file"""
+
+
+def append_write(filename="", text=""):
+    """Appends a string to a text file
+    and returns the number of characters added"""
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
