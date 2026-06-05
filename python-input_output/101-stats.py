c5a8e3c8acc03eeb9371e9f76b4502974bb2ea28
XXRV0423 <xxavierrv@gmail.com>
2026-06-05 11:56:15 -0400

    Add log parsing script for HTTP metrics

---
+++ python-input_output/101-stats.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Script that reads stdin and computes metrics"""
+
+import code
+import sys
+
+
+def print_stats(total_size, status_codes):
+    """Print accumulated metrics"""
+    print("File size: {}".format(total_size))
+
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print("{}: {}".format(code, status_codes[code]))
+
+
+total_size = 0
+line_count = 0
+valid_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+status_codes = {code: 0 for code in valid_codes}
+
+try:
+    for line in sys.stdin:
+        parts = line.split()
+
+        try:
+            file_size = int(parts[-1])
+            status_code = parts[-2]
+
+            total_size += file_size
+
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        except (IndexError, ValueError):
+            pass
+
+        line_count += 1
+
+        if line_count % 10 == 0:
+            print_stats(total_size, status_codes)
+
+except KeyboardInterrupt:
+    print_stats(total_size, status_codes)
+    raise
