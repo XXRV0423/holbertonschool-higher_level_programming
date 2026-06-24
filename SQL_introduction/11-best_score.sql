684a1128966f8524f0ee9dabc33b8c37b2d50046
XXRV0423 <xxavierrv@gmail.com>
2026-06-24 16:47:14 -0400

    Script that lists all records with a score >= 10 in decending order in scond_table

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
---Lists all records with a score >= 10 in second_table ordered by score decending
+-- Lists all records with a score >= 10 in second_table ordered by score decending
 SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
