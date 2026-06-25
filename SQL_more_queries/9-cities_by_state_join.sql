d641a255e7e42ad3daa81ca1def2b85d8522ad27
XXRV0423 <xxavierrv@gmail.com>
2026-06-25 15:29:44 -0400

    Fix: states_id to states.id in JOIN condition

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Lists all the cities with their state name using JOIN
 SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN states ON cities.state_id = states_id
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
