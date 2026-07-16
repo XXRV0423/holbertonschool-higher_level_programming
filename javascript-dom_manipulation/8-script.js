ae54d6751125d71f9c4bda1c69318bd76c22cb90
XXRV0423 <xxavierrv@gmail.com>
2026-07-16 16:00:45 -0400

    Script that fetcg translated hello text and display using DOMContentLoaded

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', function() {
+    fetch('https://hellosalut.stefanbohacek.com/?lang=fr')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            document.querySelector('#hello').textContent = data.hello;
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+});
